a93baac4c44da18463309f7008cf1170dc953706
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-02-07 11:00:07 +0100

    event variant: - details-back: added explanations in code

--- hono/src/app/parts/p02evtpage/evt-persondetails.tsx
+++ hono/src/app/parts/p02evtpage/evt-persondetails.tsx
@@ -5,7 +5,7 @@ import {OOBPersonDetailModel} from "../p01oobpage/oob-person-page-model-vm";
 export const EvtPersonDetails = (props: { vm: OOBPersonDetailModel }) => (
 		<>
 			<EvtPersondetailsRow vm={props.vm}>
-				{/* Inject behavior for 'close-details-requested' event from outside (here) into EvtPersondetailsRow component.*/}
+				{/* Inject behavior for 'close-details-requested' event from here into EvtPersondetailsRow component.*/}
 				{/* Use <template> since it is not rendered by the browser and therefore does not have undesired impact on layout.*/}
 				<template
 					hx-trigger={`close-details-requested[event.detail.id === ${props.vm.id}] from:body`}
