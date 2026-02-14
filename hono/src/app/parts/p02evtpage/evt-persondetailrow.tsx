5c2b115d59126ad170da01ea5f26b86dfba925b8
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-02-14 06:47:28 +0100

    evt: detailsrow: started with eventhandlers

--- hono/src/app/parts/p02evtpage/evt-persondetailrow.tsx
+++ hono/src/app/parts/p02evtpage/evt-persondetailrow.tsx
@@ -1,17 +1,18 @@
-import {OOBPersonDetailModel} from "../p01oobpage/oob-person-page-model-vm";
+import {OOBPersonDetailModel, OOBPersonEditModel} from "../p01oobpage/oob-person-page-model-vm";
 import {EvtBackendEvents} from "./evt-hono-web-api-shared-consts";
 import {JSX} from "hono/jsx";
 
 type TrAttrs = JSX.IntrinsicElements["tr"]
+type TemplateAttrs = JSX.IntrinsicElements["template"]
 
 export const EvtPersondetailsRow = (
-	{ vm, ...attrs }: { vm: OOBPersonDetailModel } & TrAttrs
+	{ vm, children, ...attrs }: { vm: OOBPersonDetailModel } & TrAttrs
 ) => (
 		<>
 			<tr
 				id={`row-${vm.id}`}
 				style="cursor: pointer"
-				_={`on click send 'close-details-requested'(id:${vm.id}) to <body/>`}
+				_={`on click send '${Events.CLOSE_REQUESTED}'(id:${vm.id}) to <body/>`}
 				{...attrs}
 			>
 				<template
@@ -27,4 +28,20 @@ export const EvtPersondetailsRow = (
 				<td style="border-style: none"><span className="icon"><i className="material-icons">arrow_drop_up</i></span></td>
 			</tr>
 		</>
+);
+
+const Events = {
+	CLOSE_REQUESTED: 'close-details-requested',
+};
+export const EvtCloseHandler = (
+	{ cid, vm, ...attrs }: { cid: string, vm: OOBPersonEditModel } & TemplateAttrs
+) => (
+	<template
+		hx-trigger={`
+			${Events.CLOSE_REQUESTED}[event.detail.id == ${cid}] from:closest tr
+			`}
+		hx-target="closest tr"
+		hx-swap="outerHTML"
+		{...attrs}
+	></template>
 );
